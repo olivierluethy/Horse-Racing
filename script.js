ecd999e4852abf5478d2c591621317646bbd388e
Oli7000 <olivier.luethy@gmx.net>
2022-02-15 16:45:07 +0100

    Check if horse has been selected and bet is given

--- script.js
+++ script.js
@@ -1,63 +1,12 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var win = 1450;
-
-//     document.getElementById("1").style.left = 0 + "px";
-//     document.getElementById("2").style.left = 0 + "px";
-//     document.getElementById("3").style.left = 0 + "px";
-//     document.getElementById("4").style.left = 0 + "px";
-//     document.getElementById("5").style.left = 0 + "px";
-
-//     var loop = setInterval(function() {
-//         document.getElementById("1").style.left += 1 + "px";
-//         document.getElementById("2").style.left += 1 + "px";
-//         document.getElementById("3").style.left += 1 + "px";
-//         document.getElementById("4").style.left += 1 + "px";
-//         document.getElementById("5").style.left += 1 + "px";
-
-//     }, 1000)
-
-//     clearInterval(loop);
-
-//     while (parseInt(document.getElementById("1").style.left) < win ||
-//         parseInt(document.getElementById("2").style.left) < win ||
-//         parseInt(document.getElementById("3").style.left) < win ||
-//         parseInt(document.getElementById("4").style.left) < win ||
-//         parseInt(document.getElementById("5").style.left) < win) {
-
-//         document.getElementById("1").style.left += 1 + "px";
-//         document.getElementById("2").style.left += 1 + "px";
-//         document.getElementById("3").style.left += 1 + "px";
-//         document.getElementById("4").style.left += 1 + "px";
-//         document.getElementById("5").style.left += 1 + "px";
-
-
-//         // document.getElementById("1").style.left += Math.floor(Math.random() * 50) + "px";
-//         // document.getElementById("2").style.left += Math.floor(Math.random() * 50) + "px";
-//         // document.getElementById("3").style.left += Math.floor(Math.random() * 50) + "px";
-//         // document.getElementById("4").style.left += Math.floor(Math.random() * 50) + "px";
-//         // document.getElementById("5").style.left += Math.floor(Math.random() * 50) + "px";
-//     }
-
-
-//     console.log(parseInt(document.getElementById("1").style.left) < win);
-//     console.log(parseInt(document.getElementById("2").style.left) < win);
-//     console.log(parseInt(document.getElementById("3").style.left) < win);
-//     console.log(parseInt(document.getElementById("4").style.left) < win);
-//     console.log(parseInt(document.getElementById("5").style.left) < win);
-//     console.log(document.getElementById("2").style.left);
-//     console.log(document.getElementById("3").style.left);
-//     console.log(document.getElementById("4").style.left);
-//     console.log(document.getElementById("5").style.left);
-// })
 var win = 1680;
-var possition1 = 0;
-var possition2 = 0;
-var possition3 = 0;
-var possition4 = 0;
-var possition5 = 0;
+var possition1 = 10;
+var possition2 = 10;
+var possition3 = 10;
+var possition4 = 10;
+var possition5 = 10;
+var buttonClick = 0;
 
-
-function myLoop() {
+function startLoop() {
     setTimeout(function() {
         possition1 += Math.floor(Math.random() * 10) + 1;
         possition2 += Math.floor(Math.random() * 10) + 1;
@@ -88,9 +37,44 @@ function myLoop() {
             parseInt(document.getElementById("3").style.left) < win ||
             parseInt(document.getElementById("4").style.left) < win ||
             parseInt(document.getElementById("5").style.left) < win) {
-            myLoop();
+            startLoop();
         }
     }, 10)
 }
 
-myLoop();
+function startGame() {
+    buttonClick++;
+    if (buttonClick == 1) {
+        var money;
+        var horseNumber;
+        if (document.getElementById("money").value.length == 0) {
+            alert("Please enter your bet!");
+            buttonClick = 0;
+            return;
+        } else {
+            money = document.getElementById("money").value;
+            if (document.getElementById('horse1').checked) {
+                horseNumber = 1;
+                startLoop();
+            } else if (document.getElementById('horse2').checked) {
+                horseNumber = 2;
+                startLoop();
+            } else if (document.getElementById('horse3').checked) {
+                horseNumber = 3;
+                startLoop();
+            } else if (document.getElementById('horse4').checked) {
+                horseNumber = 4;
+                startLoop();
+            } else if (document.getElementById('horse5').checked) {
+                horseNumber = 5;
+                startLoop();
+            } else {
+                alert("Please select the horse!");
+                buttonClick = 0;
+                return;
+            }
+        }
+    } else {
+
+    }
+}
