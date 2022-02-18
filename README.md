1b945548163809d67d3c7972f6a354caef3ca287
Oli7000 <olivier.luethy@gmx.net>
2022-02-18 09:42:27 +0100

    Check winner and game over display

---
+++ README.md
@@ -0,0 +1,12 @@
+# Horse-Racing Game with Javascript
+Here I'll describe how I build this game and where my issues where.
+
+## Website who helped me
+Issue 1: How do I add a delay in a JavaScript loop?
+Link: https://stackoverflow.com/questions/3583724/how-do-i-add-a-delay-in-a-javascript-loop
+
+Issue 2: Generate random number between two values
+Link: https://www.codegrepper.com/code-examples/javascript/random+number+generator+1-10+javascript
+
+Issue 3: Check if a radio button
+Link: https://www.geeksforgeeks.org/how-to-check-whether-a-radio-button-is-selected-with-javascript/
--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-var win = 1680;
+var win = 1700;
 var possition1 = 10;
 var possition2 = 10;
 var possition3 = 10;
 var possition4 = 10;
 var possition5 = 10;
 var buttonClick = 0;
+var bet = 0;
+var coins = 5;
+var horseNumber = 0;
+var rounds = 1;
+
+document.getElementById("1").style.left = 0 + "px";
+document.getElementById("2").style.left = 0 + "px";
+document.getElementById("3").style.left = 0 + "px";
+document.getElementById("4").style.left = 0 + "px";
+document.getElementById("5").style.left = 0 + "px";
 
 function startLoop() {
     setTimeout(function() {
@@ -13,6 +23,16 @@ function startLoop() {
         possition3 += Math.floor(Math.random() * 10) + 1;
         possition4 += Math.floor(Math.random() * 10) + 1;
         possition5 += Math.floor(Math.random() * 10) + 1;
+
+        if (parseInt(document.getElementById("1").style.left) <= win ||
+            parseInt(document.getElementById("2").style.left) <= win ||
+            parseInt(document.getElementById("3").style.left) <= win ||
+            parseInt(document.getElementById("4").style.left) <= win ||
+            parseInt(document.getElementById("5").style.left) <= win) {
+            startLoop();
+        } else {
+            checkWinner();
+        }
 
         document.getElementById("1").style.left = possition1 + "px";
         document.getElementById("2").style.left = possition2 + "px";
@@ -31,28 +51,18 @@ function startLoop() {
         console.log(parseInt(document.getElementById("3").style.left));
         console.log(parseInt(document.getElementById("4").style.left));
         console.log(parseInt(document.getElementById("5").style.left));
-
-        if (parseInt(document.getElementById("1").style.left) < win ||
-            parseInt(document.getElementById("2").style.left) < win ||
-            parseInt(document.getElementById("3").style.left) < win ||
-            parseInt(document.getElementById("4").style.left) < win ||
-            parseInt(document.getElementById("5").style.left) < win) {
-            startLoop();
-        }
     }, 10)
 }
 
 function startGame() {
     buttonClick++;
     if (buttonClick == 1) {
-        var money;
-        var horseNumber;
         if (document.getElementById("money").value.length == 0) {
             alert("Please enter your bet!");
             buttonClick = 0;
             return;
         } else {
-            money = document.getElementById("money").value;
+            bet = document.getElementById("money").value;
             if (document.getElementById('horse1').checked) {
                 horseNumber = 1;
                 startLoop();
@@ -77,4 +87,129 @@ function startGame() {
     } else {
 
     }
+}
+
+function checkWinner() {
+    if (parseInt(document.getElementById("1").style.left) > parseInt(document.getElementById("2").style.left) &&
+        parseInt(document.getElementById("3").style.left) &&
+        parseInt(document.getElementById("4").style.left) &&
+        parseInt(document.getElementById("5").style.left)) {
+
+        if (horseNumber == 1) {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+            coins += bet * 2;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+        } else {
+            coins -= bet;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+
+            if (coins <= 0) {
+                gameover();
+            } else if (coins > 0) {
+                rounds++;
+            }
+        }
+    } else if (parseInt(document.getElementById("2").style.left) > parseInt(document.getElementById("1").style.left) &&
+        parseInt(document.getElementById("3").style.left) &&
+        parseInt(document.getElementById("4").style.left) &&
+        parseInt(document.getElementById("5").style.left)) {
+        if (horseNumber == 2) {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+            coins += bet * 2;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+        } else {
+            coins -= bet;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+
+            if (coins <= 0) {
+                gameover();
+            } else if (coins > 0) {
+                rounds++;
+            }
+        }
+    } else if (parseInt(document.getElementById("3").style.left) > parseInt(document.getElementById("1").style.left) &&
+        parseInt(document.getElementById("2").style.left) &&
+        parseInt(document.getElementById("4").style.left) &&
+        parseInt(document.getElementById("5").style.left)) {
+        if (horseNumber == 3) {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+            coins += bet * 2;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+        } else {
+            coins -= bet;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+
+            if (coins <= 0) {
+                gameover();
+            } else if (coins > 0) {
+                rounds++;
+            }
+        }
+    } else if (parseInt(document.getElementById("4").style.left) > parseInt(document.getElementById("1").style.left) &&
+        parseInt(document.getElementById("2").style.left) &&
+        parseInt(document.getElementById("3").style.left) &&
+        parseInt(document.getElementById("5").style.left)) {
+        if (horseNumber == 4) {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+            coins += bet * 2;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+        } else {
+            coins -= bet;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+
+            if (coins <= 0) {
+                gameover();
+            } else if (coins > 0) {
+                rounds++;
+            }
+        }
+    } else if (parseInt(document.getElementById("5").style.left) > parseInt(document.getElementById("1").style.left) &&
+        parseInt(document.getElementById("2").style.left) &&
+        parseInt(document.getElementById("3").style.left) &&
+        parseInt(document.getElementById("4").style.left)) {
+        if (horseNumber == 5) {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 }
+            });
+            coins += bet * 2;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+        } else {
+            coins -= bet;
+            document.getElementById("coins").innerHTML = "<img src='images/coin.png' alt=''>" + " " + coins + " " + "Coins";
+
+            if (coins <= 0) {
+                gameover();
+            } else if (coins > 0) {
+                rounds++;
+            }
+        }
+    }
+}
+
+function gameover() {
+    document.getElementById("title").style.display = "none";
+    document.getElementById("lines").style.display = "none";
+    document.getElementById("title-selecthorse").style.display = "none";
+    document.getElementById("selectHorse").style.display = "none";
+    document.body.style.backgroundColor = "black";
+    document.getElementById("gameover").style.display = "block";
+    document.getElementById("games").innerHTML = "You were able to play " + rounds + " round";
 }
